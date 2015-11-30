57e9d71b7942a6b06e29c4875e766b536b3c8289
Travis Jeffery <tj@travisjeffery.com>
2015-11-30 16:59:42 -0500

    just start mongos with configdb passed in

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-mkdir -p /var/log/mongodb
-touch /var/log/mongodb/mongos.log
-mongos --fork --configdb $MONGOS_CONFIGDB
-tail -f /var/log/mongodb/mongos.log
+mongos --configdb $MONGOS_CONFIGDB
